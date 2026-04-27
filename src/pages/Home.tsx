9a7374664667b439612db0461d74ac284be7b385
Nithesh Hariharan <nithesh2108@gmail.com>
2026-04-27 15:05:24 +0000

    fix build

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
 import { useEffect } from 'react';
-import { Link, useLocation } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 import HEAnimation from '../components/HEAnimation';
 
 declare global {
